955a0c548c9a5958bdd2e41de1783d7863c7e296
Jack Sleeman <sleeman5@cse.msu.edu>
2023-05-19 10:28:16 -0400

    MainActivity Determines Location

--- app/src/main/java/edu/msu/sleeman5/sportsfinder/MainActivity.java
+++ app/src/main/java/edu/msu/sleeman5/sportsfinder/MainActivity.java
@@ -1,14 +1,132 @@
 package edu.msu.sleeman5.sportsfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager = null;
+
+    // Current Locational Information
+    private double latitude = 0;
+    private double longitude = 0;
+
+    // Instance of Nested Class
+    private ActiveListener activeListener = new ActiveListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                // to handle the case where the user grants the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+
+        // Get the Location Manager
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                registerListeners();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerListeners();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterListeners();
+        super.onPause();
+    }
+
+    private void registerListeners() {
+        unregisterListeners();
+
+        // Criteria object
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+
+        String bestAvailable = locationManager.getBestProvider(criteria, true);
+
+        if(bestAvailable != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
+            Location location = locationManager.getLastKnownLocation(bestAvailable);
+            newLocation(location);
+        }
+    }
+
+    private void unregisterListeners() {
+        locationManager.removeUpdates(activeListener);
+    }
+
+    private void newLocation(Location location) {
+        if(location == null) {
+            return;
+        }
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    private class ActiveListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            newLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            registerListeners();
+        }
+    };
 }
