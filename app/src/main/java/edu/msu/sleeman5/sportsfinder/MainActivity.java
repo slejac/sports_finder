d54255ba045eb954c54805c4838b63ba1fd1f19f
Jack Sleeman <sleeman5@cse.msu.edu>
2023-05-19 10:36:17 -0400

    Adding Empty Activities

--- app/src/main/java/edu/msu/sleeman5/sportsfinder/MainActivity.java
+++ app/src/main/java/edu/msu/sleeman5/sportsfinder/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -14,6 +15,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -105,6 +107,26 @@ public class MainActivity extends AppCompatActivity {
 
         latitude = location.getLatitude();
         longitude = location.getLongitude();
+    }
+
+    public void onNFL(View view) {
+        Intent intent = new Intent(this, NFLActivity.class);
+        startActivity(intent);
+    }
+
+    public void onNBA(View view) {
+        Intent intent = new Intent(this, NBAActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMLB(View view) {
+        Intent intent = new Intent(this, MLBActivity.class);
+        startActivity(intent);
+    }
+
+    public void onNHL(View view) {
+        Intent intent = new Intent(this, NHLActivity.class);
+        startActivity(intent);
     }
 
     private class ActiveListener implements LocationListener {
---
+++ app/src/main/java/edu/msu/sleeman5/sportsfinder/NBAActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.sleeman5.sportsfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NBAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nbaactivity);
+    }
+}
