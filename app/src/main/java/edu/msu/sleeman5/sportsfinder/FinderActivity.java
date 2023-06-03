ef6a1d9a86f9f12716cc8ff39ee007f16e9839db
Jack Sleeman <sleeman5@cse.msu.edu>
2023-06-02 21:38:48 -0400

    Addition of Google Maps Directions

--- app/src/main/java/edu/msu/sleeman5/sportsfinder/FinderActivity.java
+++ app/src/main/java/edu/msu/sleeman5/sportsfinder/FinderActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -15,6 +17,11 @@ public class FinderActivity extends AppCompatActivity {
     private double userLongitude = 0;
     private double userLatitude = 0;
     private int type = 0;
+
+    // Data from Populator
+    private List<HashMap<String, String>> venues;
+
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,10 @@ public class FinderActivity extends AppCompatActivity {
         type = intent.getIntExtra(MainActivity.SPORT_DATA, 0);
 
         // Create List of Mappings for Each Arena
-        List<HashMap<String, String>> venues = Populator.populate(type);
+        venues = Populator.populate(type);
 
         // Distance Greater than Earth's Circumnavigation, Impossible
         double min = 50000000;
-        int index = 0;
 
         for (int i = 0; i < venues.size(); i++) {
             float[] distances = new float[1];
@@ -51,5 +57,21 @@ public class FinderActivity extends AppCompatActivity {
         viewVenue.setText(venues.get(index).get("venue"));
         TextView viewDistance = (TextView)findViewById(R.id.textDistance);
         viewDistance.setText(Integer.toString((int)min) + " miles");
+    }
+
+    public void onDirections(View view) {
+        // https://developers.google.com/maps/documentation/urls/android-intents
+        String lat = venues.get(index).get("latitude");
+        String lon = venues.get(index).get("longitude");
+        String uriString = "google.navigation:q=";
+        uriString += lat;
+        uriString += ",";
+        uriString += lon;
+        uriString += "&mode=d";
+
+        Uri gmmIntentUri = Uri.parse(uriString);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
